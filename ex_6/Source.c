6d2079268525756f2d8f948efd467b3bc1fc938b
Nina MARTIN <nina.martin@isen.yncrea.fr>
2020-12-06 19:40:41 +0100

    dépot final

--- ex_6/Source.c
+++ ex_6/Source.c
@@ -3,20 +3,41 @@
 #include<math.h>
 #include<time.h>
 
-int main()
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <ctype.h>
+#define SIZE 100
 
-{
-	int val;
-	int tab[100];
+int main() {
+
+	int Tab[SIZE];
+	int occurence = 0;
+	int nb;
+	int check = 0;
 	srand(time(NULL));
-	int* MyPtr;
-	for (int i = 0; i < 100; i++)
-	{
-		val = rand() % 20;
-		tab[i] = val;
-		printf("%d  ", tab[i]);
 
+	for (int i = 0; i < SIZE; i++) {
+		Tab[i] = (int)rand() % 100;
 	}
 
-	
+	printf("Entrez la valeur à chercher : ");
+	scanf_s("%d", &nb);
+
+	int* curseur = &Tab[0];
+
+	for (int i = 0; i < SIZE; i++) {
+		if (*curseur == nb && check == 0) {
+			printf("\nLa valeurs %d trouvee en %d", nb, i);
+			check = 1;
+		}
+		else if ((*curseur == nb && check == 1)) {
+			printf(", puis en %d", i);
+		}
+		curseur++;
+	}
+
+	if (check == 0) { printf("\nLa valeur n est pas dans le tableau.\n"); }
+	else { printf("."); }
+
 }
